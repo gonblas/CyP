33436438e7ed134b9361fd1bc1005aa52e184c1d
Gonzalo Blasco <gonzagabino@gmail.com>
2025-03-04 18:38:06 -0300

    Laboratorio OpenMP - Terminado

---
+++ Laboratorios/OpenMP/ej1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include <time.h>
+#include <omp.h>
+
+int main(int argc, char* argv[]){
+  if(argc == 1) {
+    printf("La cantidad de parámetros es incorrecta. Por favor ingrese \n");
+    return 1;
+  }
+
+  int N = atoi(argv[1]);
+  srand(time(NULL));
+  int vector[N];
+
+  for(int i = 0; i < N; i++) {
+    int number = rand() % 30 + 1;
+    printf("%d \n", number);
+    vector[i] = number;
+  }
+  int i;
+  #pragma omp parallel for schedule(dynamic) shared(N, vector)
+    for(i = 0; i < N; i++)
+    {
+      printf("Soy el hilo %d y me voy a dormir %d segundos\n", omp_get_thread_num(), vector[i]);
+      sleep(vector[i]);
+    }
+
+  return 0;
+}
---
+++ Laboratorios/OpenMP/ej2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char* argv[]){
+  if(argc == 1) {
+    printf("La cantidad de parámetros es incorrecta. Por favor ingrese \n");
+    return 1;
+  }
+
+  int N = atoi(argv[1]);
+  printf("N = %d", N);
+  int i, j, k;
+  int *m1 = (int*)malloc(N * N * sizeof(int));
+  int *m2 = (int*)malloc(N * N * sizeof(int));
+  int *result = (int*)malloc(N * N * sizeof(int));
+
+  if (!m1 || !m2 || !result) {
+      printf("Error al asignar memoria\n");
+      return 1;
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      m1[i * N + j] = rand() % 5;
+      m2[i * N + j] = rand() % 5;
+    }
+  }
+
+  #pragma omp parallel for schedule(guided, 32) shared(m1, m2, result, N)
+  for(i=0; i<N; i++)
+  {
+    for(j=0; j<N; j++) 
+    {
+      result[i,j] = 0;
+      for(k=0;k<N;k++) 
+        result[i,j] += m1[i,k] * m2[k,j];
+        printf("result[%d,%d] = %d\n", i, j, result[i,j]);
+    }
+  }
+
+  return 0;
+}
